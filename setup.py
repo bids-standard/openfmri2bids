08b60adcf6427a8bfb79eefa39003c5f3d108c67
Chris Gorgolewski <krzysztof.gorgolewski@gmail.com>
2015-12-30 06:13:14 -0800

    Updated dependencies and python version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Convert OpenfMRI dataset to BIDS.
 """
 from setuptools import find_packages, setup
 
-dependencies = ['click']
+dependencies = ['click', 'pandas', 'numpy']
 
 setup(
     name='openfmri2bids',
@@ -41,7 +41,6 @@ setup(
         'Operating System :: Unix',
         'Operating System :: Windows',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
